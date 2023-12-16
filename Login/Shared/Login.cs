aa1f11a874971d2731f11bb5762f226ef798df95
wilyc <wilyc@SATELLITE>
2023-12-15 23:11:23 -0500

    Agregar archivos de proyecto.

---
+++ Login/Shared/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Shared
+{
+    public class Login
+    {
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+    }
+}
---
+++ Login/Shared/Sesion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Shared
+{
+    public class Sesion
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Rol {  get; set; }
+    }
+}
